d6d60c1f8d7b7921015197adecc59b1d2ee23551
vitaly <vitaly@DESKTOP-OICK4O2>
2024-04-08 00:24:06 +0300

    first commit

---
+++ Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+import styled from 'styled-components';
+
+function Sidebar() {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+const SidebarStyles = styled.nav`
+
+`;
+export default Sidebar
